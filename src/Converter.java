7c1fbd5c02addf7d70074cbbe13943d6c559bc48
Frances <97236341+frncs-dc@users.noreply.github.com>
2024-03-12 22:15:14 +0800

    feat: added basic gui

---
+++ src/Converter.java
@@ -0,0 +1,20 @@
+import javax.swing.*;
+
+public class Converter extends JFrame{
+    private JButton convertBtn;
+    private JLabel title;
+    private JPanel converterPanel;
+    private JTextField inputField;
+    private JLabel outputField;
+    private JLabel inputLabel;
+    private JLabel outputLabel;
+
+    public Converter() {
+        this.setContentPane(this.converterPanel);
+        this.setTitle("Binary-16 floating point converter");
+        this.setSize(300,200);
+        this.setLocationRelativeTo(null);
+        this.setVisible(true);
+        this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+    }
+}
