d0ddb99fe287e1043c3275c541319de0c11960c3
IT3M0 <115615684+IT3M0@users.noreply.github.com>
2024-03-13 18:26:38 +0800

    added the case if there is no decimal point present in binaryNum

--- src/Output.java
+++ src/Output.java
@@ -20,13 +20,27 @@ public class Output {
         int firstInstanceofExp = binaryNum.indexOf("^");
         int firstInstanceofx = binaryNum.indexOf("x");
 
-        this.sign = String.valueOf(isNegative(binaryNum) ? 1 : 0); //if what is returned is 1, then its negative else positive
+        if (binaryNum.contains(".")){
+            //when there is a decimal point present
+            this.sign = String.valueOf(isNegative(binaryNum) ? 1 : 0); //if what is returned is 1, then its negative else positive
 
-        this.mantissa = binaryNum.substring(firstInstanceofDecimalPoint+1, firstInstanceofx); //gets everything after the decimal point until x2^stuff
+            this.mantissa = binaryNum.substring(firstInstanceofDecimalPoint+1, firstInstanceofx); //gets everything after the decimal point until x2^stuff
 
-        this.exponent = binaryNum.substring(firstInstanceofExp+1); //gets everything after the ^ sign
+            this.exponent = binaryNum.substring(firstInstanceofExp+1); //gets everything after the ^ sign
 
-        System.out.println("Sign: " +this.sign+" Exponent: "+this.exponent+" Mantissa: "+this.mantissa);
+            System.out.println("Sign: " +this.sign+" Exponent: "+this.exponent+" Mantissa: "+this.mantissa);
+        } else {
+            //-101 x 2^something
+            //if there is no decimal point
+            this.sign = String.valueOf(isNegative(binaryNum) ? 1 : 0); //if what is returned is 1, then its negative else positive
+
+            this.mantissa = binaryNum.substring(1, firstInstanceofx); //starts reading after sign/no sign then stops reading before 'x'
+
+            this.exponent = binaryNum.substring(firstInstanceofExp+1); //gets everything after the ^ sign
+
+            System.out.println("Sign: " +this.sign+" Exponent: "+this.exponent+" Mantissa: "+this.mantissa);
+        }
+        
     }
 
     public boolean isNegative(String binaryNum){
