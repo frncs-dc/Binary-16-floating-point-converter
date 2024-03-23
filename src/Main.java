5539a7eba21d7994f77e222a32b926e440463d5b
slashZyro <122466168+slashZyro@users.noreply.github.com>
2024-03-23 16:43:13 +0800

    refactor: allow cross platform GUI

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,26 @@
+import javax.swing.UIManager;
+import javax.swing.UIManager.LookAndFeelInfo;
+
 public class Main {
     public static void main(String[] args) {
-        Converter converter = new Converter();
+        try {
+            // Set Nimbus Look and Feel
+            for (LookAndFeelInfo info : UIManager.getInstalledLookAndFeels()) {
+                if ("Nimbus".equals(info.getName())) {
+                    UIManager.setLookAndFeel(info.getClassName());
+                    break;
+                }
+            }
+        } catch (Exception e) {
+            // If Nimbus is not available, you can handle the exception
+            e.printStackTrace();
+        }
+
+        // Create your Converter GUI
+        javax.swing.SwingUtilities.invokeLater(new Runnable() {
+            public void run() {
+                Converter converter = new Converter();
+            }
+        });
     }
 }
